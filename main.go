9ac35712d1161e64befa4a22806e987523f69055
abdelrahmanassem <abdoazwak55@gmail.com>
2026-09-07 15:12:51 +0300

    created teh init option

--- main.go
+++ main.go
@@ -1,11 +1,50 @@
 package main
 
-import "fmt"
-
-
-func main(){
-
-fmt.Println("My first  Mutgit commit")
-
-
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: mugit <command>")
+		return
+	}
+	command := os.Args[1]
+	if command == "init" {
+		created, err := initRepository()
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+		if created {
+			fmt.Println("Initialized empty Mugit repository")
+		} else {
+			fmt.Println("Mugit repository already exists")
+		}
+		return
+	}
+	fmt.Println("Unknown command:", command)
+}
+func initRepository() (bool, error) {
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return false, fmt.Errorf("could not get current directory: %w", err)
+	}
+	mugitPath := filepath.Join(currentDir, ".mugit")
+	info, err := os.Stat(mugitPath)
+	if err == nil {
+		if !info.IsDir() {
+			return false, fmt.Errorf(".mugit exists but is not a directory")
+		}
+		return false, nil
+	}
+	if !os.IsNotExist(err) {
+		return false, fmt.Errorf("could not check repository: %w", err)
+	}
+	err = os.Mkdir(mugitPath, 0755)
+	if err != nil {
+		return false, fmt.Errorf("could not create .mugit: %w", err)
+	}
+	return true, nil
 }
